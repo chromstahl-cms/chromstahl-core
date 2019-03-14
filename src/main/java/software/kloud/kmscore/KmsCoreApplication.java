61e92025f578585ad3fa6b48a98f48fa6285085c
lars <lars@kloud.software>
2019-03-14 13:43:57 +0100

    init

---
+++ src/main/java/software/kloud/kmscore/KmsCoreApplication.java
@@ -0,0 +1,13 @@
+package software.kloud.kmscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmsCoreApplication.class, args);
+	}
+
+}
